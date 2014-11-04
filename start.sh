94737a249aa6995a3ad142328724cd5162a1d02d
Sascha Marcel Schmidt <git@saschaschmidt.net>
2014-11-04 14:42:48 +0100

    unified start scripts

--- start.sh
+++ start.sh
@@ -2,4 +2,10 @@
 cd /var/www/redmine
 rake generate_secret_token
 RAILS_ENV=production rake db:migrate
-exec /run-apache.sh
+
+# Make sure we're not confused by old, incompletely-shutdown httpd
+# context after restarting the container.  httpd won't start correctly
+# if it thinks it is already running.
+rm -rf /run/httpd/*
+
+exec /usr/sbin/apachectl -D FOREGROUND
